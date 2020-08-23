10e20d2b25b67f08a912c6c5af951bd7c070f5a4
Andre Goranico Samosir <abrammarba@gmail.com>
2020-08-24 00:32:36 +0700

    Update the code.

--- README.md
+++ README.md
@@ -3,6 +3,26 @@ Scrap some data from owgr.com.
 
 This project is related to rolexrankings in https://github.com/andre-gnc/rolexrankings. As mentioned there, it's a web scraper job offer from Upwork. Image file named screencapture-upwork-jobs-01578ee58f5ddae5d1-2020-08-17-11_32_20.png shows the details.
 
+The deliverable must be like file named Experience data.docx.
+
 Before, I scraped from rolexrankings.com. Here, it's owgr.com that I scraped.
 
- 
+This was much easier. I just used Requests and BeautifulSoup.
+
+If you just test the code, I suggest to set the "break" in line 87. But if you want to run it fully then set the "break" as comment (# break). There was 8000+ records.
+
+I basically had coded it as good as I can. Nevertheless, I realize that it's far from perfect. Please give me your advices. Thank you.
+
+==========
+
+Proyek ini berhubungan dengan rolexrankings di https://github.com/andre-gnc/rolexrankings. Sebagaimana disebutkan di sana, ini adalah tawaran pekerjaan tentang web scraper dari Upwork. Fail gambar screencapture-upwork-jobs-01578ee58f5ddae5d1-2020-08-17-11_32_20.png menjelaskan rinciannya.
+
+Hasilnya harus seperti fail Experience data.docx.
+
+Sebelumnya, saya mengerjakan scrap dari rolexrankings.com. Sekarang, owgr.com yang dikerjakan.
+
+Kali ini jauh lebih mudah. Saya hanya menggunakan Requests and BeautifulSoup.
+
+Kalau hanya mengetes kodenya, saya sarankan mengeset "break" di baris 87. Tapi kalau mau dijalankan penuh, "break"-nya harus diset sebagai komentar (# break). Ada lebih dari 8000 data di sana.
+
+Saya pada dasarnya sudah menyusun kode sebaik-baiknya. Meski demikian, saya sadar ini jauh dari sempurna. Tolong beri masukan. Terima kasih.
--- main.py
+++ main.py
@@ -1,28 +1,45 @@
 import requests
+import xlsxwriter
 from bs4 import BeautifulSoup
 
-# Souping the main url.
+
+def make_soup(ms_url):
+    # Make soups of each url.
+    response = requests.get(ms_url)
+    data = response.text
+    ms_soup = BeautifulSoup(data, 'html.parser')
+    return ms_soup
+
+
+def xlsxwriter_write(xw_items, xw_index_row, xw_worksheet, xw_start, xw_format):
+    for xw_counter_col, xw_item in enumerate(xw_items, xw_start):
+        xw_worksheet.write(xw_index_row, xw_counter_col, xw_item, xw_format)
+
+
+GENDER = 'Male'
+
+workbook = xlsxwriter.Workbook('data.xlsx')
+worksheet = workbook.add_worksheet()
+bold = workbook.add_format({'bold': True})
+index_row = 0
+
+# Write the headers.
+col_headers = ('Player ID', 'Player Name', 'Last name', 'Other Name', 'Gender', 'Year', 'Events Played', '1st',
+               '2nd', '3rd', '4th-10th', 'MC', 'Year End Rank')
+xlsxwriter_write(col_headers, index_row, worksheet, 0, bold)
+
 url_main = 'http://www.owgr.com/ranking?pageNo=1&pageSize=All&country=All'
-response = requests.get(url_main)
-data = response.text
-soup = BeautifulSoup(data, 'html.parser')
+
+soup = make_soup(url_main)
 
 # Find the urls of each players.
 urls_players = soup.find('table').find_all('a')
-# print(len(urls_players))
-# Records: 8869
-i = 0
-for url_players in urls_players:
-    # Monitor the progress.
-    i += 1
-    print(i)
-    # print('http://www.owgr.com/' + url_players.get('href'))
-    # Tested. Good.
 
-    # Souping each url of players.
-    response_player = requests.get('http://www.owgr.com/' + url_players.get('href'))
-    data_player = response_player.text
-    soup_player = BeautifulSoup(data_player, 'html.parser')
+for counter_up, url_players in enumerate(urls_players, 1):
+    # Monitor the progress.
+    print(counter_up)
+
+    soup_player = make_soup('http://www.owgr.com/' + url_players.get('href'))
 
     # Grab player's identity.
     href = url_players.get('href')
@@ -32,31 +49,40 @@ for url_players in urls_players:
     position_space = player_name.rfind(' ')
     p_lastname = player_name[position_space + 1:]
     p_othername = player_name[:position_space]
-    # print(player_id, player_name, p_lastname, p_othername)
+
+    value_cols = [player_id, player_name, p_lastname, p_othername, GENDER]
 
     # Find the required table.
     tables = soup_player.find_all('table')
-    # print(len(tables))
-    # Records: 3
-    i_table = 0
-    for table in tables:
-        # Skip the first 2 tables.
-        if i_table != 2:
-            i_table += 1
+    for counter_tables, table in enumerate(tables, 1):
+        # The required table is the 3rd.
+        if counter_tables != 3:
             continue
         else:
-            print(table)
             rows = table.find_all('tr')
-            i_row = 0
-            for row in rows:
-                # Skip the last row (total).
-                if i_row + 1 == len(rows):
-                    break
-                # Skip the first row (header).
-                elif i_row == 0:
-                    i_row += 1
+
+            # Remove the last row (Total).
+            rows.pop()
+
+            for counter_row, row in enumerate(rows, 1):
+                # The required rows are from the 2nd.
+                # Skip the header.
+                if counter_row == 1:
                     continue
                 else:
-                    i_row += 1
-                    # Belum selesai di sini. ==================================
+                    index_row += 1
+
+                    # Write the values of the first 5 columns.
+                    xlsxwriter_write(value_cols, index_row, worksheet, 0, None)
+
+                    column_urls = row.find_all('td')
+                    columns = []
+                    # Get the content as text from each column.
+                    for column_url in column_urls:
+                        columns.append(column_url.text)
+
+                    # Write the last values of other columns from the 6th.
+                    # It's 5 since start at 0.
+                    xlsxwriter_write(columns, index_row, worksheet, 5, None)
     break
+workbook.close()
